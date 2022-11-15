e977deb6de98497fac2ef9d173b103df90814387
Karthikeyan C <92738999+Karthi-07@users.noreply.github.com>
2022-11-15 20:34:45 +0530

    Add files via upload

---
+++ ViewUser.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class ViewUser extends JFrame implements ActionListener
+{
+    public ViewUser()
+    {
+        
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+
+    }
+    public static void main()
+    {
+
+    }
+} 
